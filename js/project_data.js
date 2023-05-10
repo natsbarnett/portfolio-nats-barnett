cd2f60656b37e7fea7721700f623ca8f29fae49d
Nats Barnett <111586282+natsbarnett@users.noreply.github.com>
2023-05-10 20:54:31 +0200

    damn, js

---
+++ js/project_data.js
@@ -0,0 +1,6 @@
+const projects = {
+    "luxiemdatingsim": {
+        "title": "Light The Way • Luxiem Dating Sim",
+        "desc": "I have been part of the project \"Light The Way • Luxiem Dating Sim\" in Simporiem Studios as Mysta's route programmer. This Dating Sim has been made with Ren'Py a Python based programming language.",
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,7 @@
+let param = new URLSearchParams(location.search);
+let project = param.get('project');
+
+document.querySelector(".title_proj").innerHTML = "<h2 class=\"title_project\">" + projects[project]["title"] + " • </h2>";
+document.querySelector(".title_proj2").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["title"] + "</h2>";
+document.querySelector(".title_proj3").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["title"] + "</h2>";
+document.querySelector(".desc_proj").innerHTML = "<p class=\"para_proj\">" + projects[project]["desc"] + "</p>";
