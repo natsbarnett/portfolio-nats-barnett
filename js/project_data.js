dea7b6c18ae302f23d806d18a1bdf7618bca003d
Nats Barnett <111586282+natsbarnett@users.noreply.github.com>
2023-05-11 15:26:35 +0200

    damn

--- js/project_data.js
+++ js/project_data.js
@@ -2,7 +2,7 @@ const projects = {
     "luxiemdatingsim": {
         "title": "Light The Way • Luxiem Dating Sim",
         "desc": "I have been part of the project \"Light The Way • Luxiem Dating Sim\" in Simporiem Studios as Mysta's route programmer. This Dating Sim has been made with Ren'Py a Python based programming language.",
-        "link": "<iframe class=\"frame\" frameborder=\"0\" src=\"https://itch.io/embed/1843062\" width=\"552\" height=\"167\"><a href=\"https://simporiem-studios.itch.io/light-the-way\" target=\"_blank\">Light the Way by Simporiem Studios</a></iframe>",
+        "link": "<iframe class=\"frame\" frameborder=\"0\"  src=\"https://itch.io/embed/1843062?border_width=0&amp;bg_color=191817&amp;fg_color=ffffff&amp;link_color=bdbdbd&amp;border_color=191817\" width=\"550\" height=\"165\">><a href=\"https://simporiem-studios.itch.io/light-the-way\" target=\"_blank\">Light the Way by Simporiem Studios</a></iframe>",
         "img": "<img src=\"../img/ltwlds_im2.png\" width=\"50%\" class=\"img_proj\">",
     },
     "optionguide": {
@@ -21,12 +21,24 @@ const projects = {
         "title": "Light the Way OP 【Promo MV】• French TL",
         "desc": "As we prepared the closed captions of the original version, we also prepared some translations. I have been working on the French Translation of this MV.",
         "link": "<iframe class=\"frame\" width=\"720\" height=\"360\" src=\"https://www.youtube.com/embed/rXX2gnxzVCg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>",
-        "img": "",
+        "img": "<img src=\"../img/op_tl_meme.jpg\" width=\"25%\" class=\"img_proj\">",
     },
     "clips": {
         "title": "Clipping Vtubers",
         "desc": "In my free time, I have the habit to clip some VTubers I enjoy watching.",
         "link": "<iframe class=\"frame\" width=\"720\" height=\"360\" src=\"https://www.youtube.com/embed/5BAY9KFlGH4\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>",
-        "img":"",
+        "img": "<img src=\"../img/meme_clip.jpg\" width=\"25%\" class=\"img_proj\">",
+    },
+    "portfolio": {
+        "title": "My Portfolio",
+        "desc": "To put all my work inside the same place, I decided to make a portfolio. This website is still not responsive but I'm working on it as much as I can! I am trying to put all of my knowledge I acquire while in school into this website. Maybe one day, this website will be available and adapted for mobile screens, for now, it's preferable to visit it on pc screens.",
+        "link": "",
+        "img": "<img src=\"../img/portfolio_img.png\" width=\"50%\" class=\"img_proj\">",
+    },
+    "luxcardg": {
+        "title": "Luxiem Card Game",
+        "desc":"For fun, back in november 2022, I decided to create a card game on Luxiem, the game has a few rules found in a little handbook and a set of 136 base cards and questions. The unseiso expansion adds 24 cards and 2 special tweet cards.",
+        "link": "<iframe class =\"frame\" frameborder=\"0\" src=\"https://itch.io/embed/1771013?border_width=0&amp;bg_color=191817&amp;fg_color=ffffff&amp;link_color=bdbdbd&amp;border_color=191817\" width=\"550\" height=\"165\"><a href=\"https://natsbarnett.itch.io/luxiem-card-game\">Luxiem Card Game by Nats Barnett</a></iframe>",
+        "img":"<img src=\"../img/lux_card_g.png\" width=\"50%\" class=\"img_proj\">",
     },
 }
