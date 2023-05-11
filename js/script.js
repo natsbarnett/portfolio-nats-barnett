46b4d76ec48b6b23205bee4102b154547ddecede
Nats Barnett <111586282+natsbarnett@users.noreply.github.com>
2023-05-11 09:59:49 +0200

    js go brrr

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let param = new URLSearchParams(location.search);
 let project = param.get('project');
 
-document.querySelector(".title_proj").innerHTML = "<h2 class=\"title_project\">" + projects[project]["title"] + " • </h2>";
-document.querySelector(".title_proj2").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["title"] + "</h2>";
-document.querySelector(".title_proj3").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["title"] + "</h2>";
+document.querySelector(".title_proj").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["title"] + " • "+ projects[project]["title"] + " • "+ projects[project]["title"] +" • </h2>";
+document.querySelector(".title_proj2").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["title"] + " • "+ projects[project]["title"] + " • "+ projects[project]["title"] + " • </h2>";
 document.querySelector(".desc_proj").innerHTML = "<p class=\"para_proj\">" + projects[project]["desc"] + "</p>";
