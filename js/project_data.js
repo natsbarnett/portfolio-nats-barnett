fdc3d2ea07cdb697a0660b56292908a58e61b165
Nats Barnett <111586282+natsbarnett@users.noreply.github.com>
2023-05-11 14:34:01 +0200

    oof

--- js/project_data.js
+++ js/project_data.js
@@ -2,5 +2,31 @@ const projects = {
     "luxiemdatingsim": {
         "title": "Light The Way • Luxiem Dating Sim",
         "desc": "I have been part of the project \"Light The Way • Luxiem Dating Sim\" in Simporiem Studios as Mysta's route programmer. This Dating Sim has been made with Ren'Py a Python based programming language.",
-    }
+        "link": "<iframe class=\"frame\" frameborder=\"0\" src=\"https://itch.io/embed/1843062\" width=\"552\" height=\"167\"><a href=\"https://simporiem-studios.itch.io/light-the-way\" target=\"_blank\">Light the Way by Simporiem Studios</a></iframe>",
+        "img": "<img src=\"../img/ltwlds_im2.png\" width=\"50%\" class=\"img_proj\">",
+    },
+    "optionguide": {
+        "title": "Luxiem Dating Sim • Option Guide",
+        "desc": "In order to present the options of the game \"Light The Way • Luxiem Dating Sim\", I participated into making an online guide. A website entirely made by hand.",
+        "link": "<a href = \"https://natsbarnett.github.io/luxiem-dating-sim-options-guide/\" class=\"linkd\" target=\"_blank\">Link to the website</a>",
+        "img": "<img src=\"../img/option_guide_im.png\" width=\"50%\" class=\"img_proj\">",
+    },
+    "genshintest": {
+        "title": "Genshin Impact Experience Test",
+        "desc": "For fun, I decided to create a Genshin Test. This was made with HTML, CSS and some JavaScript to calculate the final score and give a meaning to the results. This has been inspired by this test: <a href= \"https://hakuhakuwu.github.io/How-Messed-Up-is-Your-Fanfiction-Taste/fanfictiontest.html\" class=\"wh_link\">How Messed Up Is Your Fanfiction Taste</a>",
+        "link": "<a href=\"https://natsbarnett.github.io/genshin-test/\" class=\"linkd\" target=\"_blank\">Link to the website</a>",
+        "img": "<img src=\"../img/ft_test_im.png\" width=\"50%\" class=\"img_proj\">",
+    },
+    "ltwoptl": {
+        "title": "Light the Way OP 【Promo MV】• French TL",
+        "desc": "As we prepared the closed captions of the original version, we also prepared some translations. I have been working on the French Translation of this MV.",
+        "link": "<iframe class=\"frame\" width=\"720\" height=\"360\" src=\"https://www.youtube.com/embed/rXX2gnxzVCg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>",
+        "img": "",
+    },
+    "clips": {
+        "title": "Clipping Vtubers",
+        "desc": "In my free time, I have the habit to clip some VTubers I enjoy watching.",
+        "link": "<iframe class=\"frame\" width=\"720\" height=\"360\" src=\"https://www.youtube.com/embed/5BAY9KFlGH4\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>",
+        "img":"",
+    },
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 let param = new URLSearchParams(location.search);
 let project = param.get('project');
 
-document.querySelector(".title_proj").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["title"] + " • "+ projects[project]["title"] + " • "+ projects[project]["title"] +" • </h2>";
-document.querySelector(".title_proj2").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["title"] + " • "+ projects[project]["title"] + " • "+ projects[project]["title"] + " • </h2>";
+/*Filter*/
+document.querySelectorAll("input").forEach(e => { e.addEventListener("change", filtre) });
+
+function filtre() {
+    document.querySelector(".resultat").classList.toggle(this.name);
+}
+document.querySelector("title").innerText="Project • " + projects[project]["title"];
+
+/*Filling the page of the projects with JS*/
+document.querySelector(".title_proj").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["title"] + " • " + projects[project]["title"] + " • " + projects[project]["title"] + " • </h2>";
+document.querySelector(".title_proj2").innerHTML = "<h2 class=\"title_project\">" + " " + projects[project]["title"] + " • " + projects[project]["title"] + " • " + projects[project]["title"] + " • </h2>";
+document.querySelector(".title_proj3").innerHTML = "<h2 class=\"txti\">" + " " + projects[project]["title"] + " • " + projects[project]["title"] + " • " + projects[project]["title"] + " • </h2>";
+document.querySelector(".title_proj4").innerHTML = "<h2 class=\"txti\">" + " " + projects[project]["title"] + " • " + projects[project]["title"] + " • " + projects[project]["title"] + " • </h2>";
+
 document.querySelector(".desc_proj").innerHTML = "<p class=\"para_proj\">" + projects[project]["desc"] + "</p>";
+document.querySelector(".link").innerHTML = projects[project]["link"];
+document.querySelector(".images").innerHTML = projects[project]["img"];
+document.querySelector(".proj_title>h1").innerText = projects[project]["title"]
